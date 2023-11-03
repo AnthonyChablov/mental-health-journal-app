2e0c857ac1c2fc3b45771300418501bace4eb282
Anthony Chablov <achablov@myseneca.ca>
2023-11-03 14:48:03 -0400

    update journalFilter State

--- client/src/store/useJournalFilterStore.ts
+++ client/src/store/useJournalFilterStore.ts
@@ -1,8 +1,8 @@
 import create from "zustand";
-import { IJournalFilter } from "@/models/journalModels";
 
 // Define the existing state and actions
 type State = {
+  filterMode: "Title" | "Date" | "Mood" | "Content" | undefined;
   filterTitle: "asc" | "desc" | "clear" | undefined;
   filterDate: "asc" | "desc" | "clear" | undefined;
   filterContent: "asc" | "desc" | "clear" | undefined;
@@ -10,6 +10,9 @@ type State = {
 };
 
 type Actions = {
+  setFilterMode: (
+    filterMode: "Title" | "Date" | "Mood" | "Content" | undefined
+  ) => void;
   setFilterTitle: (filterTitle: "asc" | "desc" | "clear" | undefined) => void;
   setFilterDate: (filterDate: "asc" | "desc" | "clear" | undefined) => void;
   setFilterContent: (
@@ -19,10 +22,12 @@ type Actions = {
 };
 
 export const useJournalFilterStore = create<State & Actions>((set) => ({
+  filterMode: undefined,
   filterTitle: undefined, // Initial state values
   filterDate: undefined,
   filterContent: undefined,
   filterMood: undefined,
+  setFilterMode: (newFilterMode) => set({ filterMode: newFilterMode }),
   setFilterTitle: (newFilterTitle) => set({ filterTitle: newFilterTitle }),
   setFilterDate: (newFilterDate) => set({ filterDate: newFilterDate }),
   setFilterContent: (newFilterContent) =>
