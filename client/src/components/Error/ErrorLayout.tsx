c2385804ba2804b54d0062dc5f4eea8dbafec8b0
Anthony Chablov <achablov@myseneca.ca>
2024-01-15 08:31:43 -0500

    feat: create loader

---
+++ client/src/components/Error/ErrorLayout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorLayout = () => {
+  return <div>ErrorLayout</div>;
+};
+
+export default ErrorLayout;
--- client/src/components/Loading/LoadingLayout.tsx
+++ client/src/components/Loading/LoadingLayout.tsx
@@ -1,9 +1,29 @@
-import React from "react";
-import { Spinner } from "@nextui-org/spinner";
+import { useState, CSSProperties } from "react";
+import MoonLoader from "react-spinners/MoonLoader";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
+
 const LoadingLayout = () => {
+  let [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#6d527d");
+
   return (
-    <div className="h-screen  w-screen bg-skin">
-      <Spinner label="Loading..." color="danger" />
+    <div className=" h-screen w-full bg-skin flex flex-col items-center justify-center">
+      <MoonLoader
+        color={color}
+        loading={loading}
+        cssOverride={override}
+        size={45}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+      <h1 className=" text-dark-purple font-semibold text-xl mt-4">
+        Loading...
+      </h1>
     </div>
   );
 };
